5efb19d66769275f08cfd8ce6b4f6a8cba4839fc
Lima-Almeida <m242827@dac.unicamp.br>
2024-06-08 20:19:01 -0300

    Add imagens e seus vetores

--- python/debug.py
+++ python/debug.py
@@ -1,21 +1,25 @@
 import numpy as np
 from PIL import Image
 
-path = input("Digite o caminho: ")
+path = input("Digite o nome da imagem: ")
 
-w, h = Image.open(path).size
+w, h = Image.open(f"{path}.png").size
 
 print(f"Img:{w * h}")
 print()
 
-with open("R_out.txt", 'r') as file:
+with open(f"R_out_{path}.txt", 'r') as file:
     contents = file.read()
     print(f"R:{len(contents)}") 
 
-with open("G_out.txt", 'r') as file:
+with open(f"G_out_{path}.txt", 'r') as file:
     contents = file.read()
     print(f"G:{len(contents)}")  
 
-with open("B_out.txt", 'r') as file:
+with open(f"B_out_{path}.txt", 'r') as file:
     contents = file.read()
     print(f"B:{len(contents)}")
+
+with open(f"Simplified_out_{path}.txt", 'r') as file:
+    contents = file.read()
+    print(f"Simplified:{len(contents)}")
--- python/images.py
+++ python/images.py
@@ -1,18 +1,19 @@
 import numpy as np
 from PIL import Image
 
-path = input("Digite o caminho: ")
+path = input("Digite o nome da imagem: ")
 
-img = Image.open(path).convert('RGB')
+img = Image.open(f"{path}.png").convert('RGB')
 arr = np.array(img)
 
 # R = list()
 # G = list()
 # B = list()
 
-R = open("R_out.txt", "w")
-G = open("G_out.txt", "w")
-B = open("B_out.txt", "w")
+R = open(f"R_out_{path}.txt", "w")
+G = open(f"G_out_{path}.txt", "w")
+B = open(f"B_out_{path}.txt", "w")
+S = open(f"Simplified_out_{path}.txt", "w")
 
 for i in range(len(arr)):
     for j in range(len(arr[0])):
@@ -28,7 +29,12 @@ for i in range(len(arr)):
             B.write('0')
         else:
             B.write('1')
+        if not arr[i][j][0] == 0 or not arr[i][j][1] == 0 or not arr[i][j][2] == 0:
+            S.write('1')
+        else:
+            S.write('0')
 
 R.close()
 G.close()
 B.close()
+S.close()
