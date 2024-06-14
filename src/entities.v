4ba8941a9f9e5eb1403109ada29e277aefd84d41
Vinicius Patriarca Miranda Miguel <ra260731@zecolmeia.lab.ic.unicamp.br>
2024-06-14 09:46:35 -0300

    5 inimigos atirando

--- src/entities.v
+++ src/entities.v
@@ -5,9 +5,9 @@ module entities(
     input [3:0] keysout,
     output [9:0] x_bola_aliada,
     output [9:0] y_bola_aliada, 
-    output [49:0] raio_bola_aliada, 
+    output [9:0] raio_bola_aliada, 
     output [49:0] x_bola_inimiga,
-    output [9:0] y_bola_inimiga,
+    output [49:0] y_bola_inimiga,
     output [9:0] raio_bola_inimiga,
     output [9:0] x_nave,
     output [9:0] y_nave,
@@ -16,11 +16,8 @@ module entities(
     output [49:0] inimigo_y,
     output [0:4] inimigo_vivo_array 
 );
-    // assign x_bola_inimiga = 500;
-    // assign y_bola_inimiga = 100;
     assign raio_bola_inimiga = 5;
 
-    // assign Reset = reset || reiniciarJogo;
     wire [9:0] xi_bola;
     wire [9:0] yi_bola;
     assign xi_bola = x_nave + (45 / 2);
@@ -48,11 +45,6 @@ module entities(
         .y_nave(y_nave),
     );
 
-
-    // wire [49:0] inimigo_x;
-    // wire [49:0] inimigo_y;
-    // wire [4:0] inimigo_vivo_array;
-
     fileira controladorInimigosInstancia(
         .CLOCK_50(CLOCK_50),
         .CLOCK_MV(CLOCK_MV),
@@ -67,27 +59,6 @@ module entities(
         .reg_y_bola(y_bola_inimiga),
         .reg_vivo(inimigo_vivo_array)
     );
-    // Atribuindo a posição do primeiro inimigo da fileira
-    // assign x_inimigo = inimigo_x[9:0];
-    // assign y_inimigo = inimigo_y[9:0];
-    // assign inimigo_vivo = inimigo_vivo_array[0];
-
-    // inimigo inimigoInstancia(
-    //     .CLOCK_50(CLOCK_50),
-    //     .reset(reset),
-    //     .pausa(pausa), 
-    //     .reiniciarJogo(0),
-
-    //     .xi(20),
-    //     .yi(40),
-
-    //     .x(x_inimigo),
-    //     .y(y_inimigo),
-
-    //     .bola_nave_x(x_bola_aliada),
-    //     .bola_nave_y(y_bola_aliada),
-    //     .vivo(inimigo_vivo)
-    // );
 
     bola bolaAliada(
         .CLOCK_50(CLOCK_50),
@@ -96,7 +67,6 @@ module entities(
         .reiniciarJogo(0),
         .xi(xi_bola),
         .yi(yi_bola),
-        // .sentidoY(1),
         .x(x_bola_aliada),
         .y(y_bola_aliada),
         .raio(raio_bola_aliada)
